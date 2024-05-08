bb574d38aa5f8d0d228f528f033eb7cfd3715c10
Haim Sarraf <138432854+HaimSarraf@users.noreply.github.com>
2024-05-08 12:14:03 +0330

    Update Meats.tsx

--- src/components/meat/Meats.tsx
+++ src/components/meat/Meats.tsx
@@ -9,6 +9,11 @@ import "./Meats.scss";
 
 const Meats = () => {
   const allProducts = useSelector((state: RootState) => state.product.products);
+  const cartItems = useSelector((state: RootState) => state.product.cartItems);
+
+  const selectedCartItems = cartItems.filter(
+    (product) => product.category === "meat"
+  );
 
   const selectedProducts = allProducts.filter(
     (product) => product.category === "meat"
@@ -34,7 +39,16 @@ const Meats = () => {
                   >
                     -
                   </button>
-                  <p>{product.quantity}</p>
+                  {selectedCartItems.find((item) => item.id === product.id) ? (
+                    <p>
+                      {
+                        selectedCartItems.find((item) => item.id === product.id)
+                          ?.quantity
+                      }
+                    </p>
+                  ) : (
+                    <p>{product.quantity}</p>
+                  )}
                   <button
                     onClick={() => {
                       dispatch(increaseQuantity(product.id));
